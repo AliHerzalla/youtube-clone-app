9e3d515a3a07d34241424872f51e6307e12435c8
AliHerzalla <93243926+AliHerzalla@users.noreply.github.com>
2023-01-28 17:49:56 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Box } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Feed,NavBar,VideoDetail,ChannelDetail,SearchFeed} from './components'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+    <BrowserRouter>
+        <Box>
+            <NavBar/>
+            <Routes>
+                <Route path='/' exact element={<Feed/>}/>
+                <Route path='/video/:id' element={<VideoDetail/>}/>
+                <Route path='/channel/:id' element={<ChannelDetail/>}/>
+                <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
+            </Routes>
+        </Box>
+    </BrowserRouter>
+);
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Feed from "./Feed";
+import ChannelDetail from "./ChannelDetail";
+import NavBar from "./NavBar";
+import SearchFeed from "./SearchFeed";
+import VideoDetail from "./VideoDetail";
+
+const Pages = { Feed, NavBar, SearchFeed, VideoDetail, ChannelDetail }
+
+export default Pages;
+
